19045f2b9d6e7815098b5def009a85b48a5b7e19
Swapneel Bobba <swapneel.bobba@proton.me>
2023-08-19 00:46:21 +0400

    first commit: setup

---
+++ tailwind.config.ts
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    // "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        orangePrimary: "#FF862D",
+        orangeLight: "#FFC092",
+        beige: "#F9F4F2",
+      },
+    },
+  },
+  plugins: [],
+};
