2d6567b9aeeef6d980e58e16357d0b373bd769a6
Swapneel Bobba <swapneel.bobba@proton.me>
2023-08-24 12:19:52 +0400

    change to erc721 and update to polygon contracts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
-import { Goerli } from "@thirdweb-dev/chains";
+import { Polygon } from "@thirdweb-dev/chains";
 import { type TBAccountParams } from "@tokenbound/sdk/dist/src/TokenboundClient";
 
 export const DEFAULT_ACCOUNT: TBAccountParams = {
@@ -12,10 +12,16 @@ export const ACCESS_PASS_ADDRESS_SCROLL =
   "0xfc29c0118c79d657885Ef7802dD595475bfA169f";
 export const ACCESS_PASS_ADDRESS_GOERLI =
   "0x321940edED4AF0Cc35a593cb9c1675Fd63c7A7cf";
-export const ACTIVE_CHAIN = Goerli;
+export const ACCESS_PASS_ADDRESS_POLYGON =
+  "0xe09d8fC773B45Dbd71f7d1744e5E4De0a24Ab509";
+
+export const ACTIVE_CHAIN = Polygon;
 
 export const FACTORY_ADDRESS_GOERLI =
   "0x02101dfB77FDE026414827Fdc604ddAF224F0921";
 
 export const IMPLEMENTATION_ADDRESS_GOERLI =
   "0xd5c3EAA2fA812c3a45a773d4962557765B6E0ecD";
+
+export const IMPLEMENTATION_ADDRESS_POLYGON =
+  "0x2d25602551487c3f3354dd80d76d54383a243358";
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,23 +8,21 @@ import {
   useWallet,
   Web3Button,
 } from "@thirdweb-dev/react";
-import styles from "@/styles/Home.module.css";
+
 import Image from "next/image";
 import { NextPage } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
-import accessPassImgLg from "@/public/images/access-pass-lg.png";
 import accessPassImg from "@/public/images/access-pass.png";
 import {
-  ACCESS_PASS_ADDRESS_GOERLI,
-  IMPLEMENTATION_ADDRESS_GOERLI,
+  ACCESS_PASS_ADDRESS_POLYGON,
+  IMPLEMENTATION_ADDRESS_POLYGON,
 } from "@/lib/constants";
-import { BigNumber, Signer } from "ethers";
+import { Signer } from "ethers";
 import toast from "react-hot-toast";
 import { useState } from "react";
 import { newSmartWallet } from "@/lib/utils/smartwallet";
 import { TokenboundClient } from "@tokenbound/sdk";
-import { DEFAULT_ACCOUNT } from "@/lib/constants";
-import { ACTIVE_CHAIN } from "../lib/constants";
+import { ACTIVE_CHAIN } from "@/lib/constants";
 
 const bricolage = Bricolage_Grotesque({ subsets: ["latin"] });
 
@@ -38,7 +36,7 @@ const Home: NextPage = () => {
   const address = useAddress();
   const wallet = useWallet();
 
-  const { contract } = useContract(ACCESS_PASS_ADDRESS_GOERLI);
+  const { contract } = useContract(ACCESS_PASS_ADDRESS_POLYGON);
   const { data: nft, isLoading, error } = useNFT(contract, 0);
 
   const {
@@ -68,11 +66,11 @@ const Home: NextPage = () => {
   const tokenboundClient = new TokenboundClient({
     signer,
     chainId: ACTIVE_CHAIN.chainId,
-    implementationAddress: IMPLEMENTATION_ADDRESS_GOERLI as `0x${string}`,
+    implementationAddress: IMPLEMENTATION_ADDRESS_POLYGON as `0x${string}`,
   });
 
   const tokenboundAccount = tokenboundClient.getAccount({
-    tokenContract: ACCESS_PASS_ADDRESS_GOERLI,
+    tokenContract: ACCESS_PASS_ADDRESS_POLYGON,
     tokenId: "0",
   });
 
@@ -94,13 +92,13 @@ const Home: NextPage = () => {
         <Image src={accessPassImg} alt="Access Pass" className="max-w-xl" />
       </div>
       <div>
-        {address && nft ? (
+        {address && nft && nftBalance && nftBalance?.toNumber() < 1 ? (
           <Web3Button
             className="px-6 py-3 rounded-lg bg-bluePrimary text-white hover:bg-blue-500 transition-colors duration-500 animate-in fade-in-10"
-            contractAddress={ACCESS_PASS_ADDRESS_GOERLI}
+            contractAddress={ACCESS_PASS_ADDRESS_POLYGON}
             action={async (contract) => {
               try {
-                await contract?.erc1155.claim(0, 1);
+                await contract?.erc721.claimTo(address, 1);
               } catch (err: unknown) {
                 console.error("error claiming: ", err);
               }
